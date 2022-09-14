a5b812231c615ff1e869a6b85a213c257a46082e
rcoric3 <92095914+rcoric3@users.noreply.github.com>
2022-09-14 14:11:46 +0200

    added components

--- my-app/src/TaskPage.js
+++ my-app/src/TaskPage.js
@@ -1,7 +1,19 @@
 import "./App.css";
 
 function TaskPage() {
-  return <div className="TaskPage"></div>;
+  return (
+    <div className="TaskPage">
+      <label>
+        <ul>
+          <li>1</li>
+          <li>1</li>
+          <li>1</li>
+          <li>1</li>
+          <li>1</li>
+        </ul>
+      </label>
+    </div>
+  );
 }
 
 export default TaskPage;
