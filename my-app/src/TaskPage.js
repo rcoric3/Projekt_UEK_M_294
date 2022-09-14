1648d130cf28128d39d8aa1100c1fb46e2df9172
rcoric3 <92095914+rcoric3@users.noreply.github.com>
2022-09-14 13:21:17 +0200

    added database

---
+++ my-app/src/TaskPage.js
@@ -0,0 +1,7 @@
+import "./App.css";
+
+function TaskPage() {
+  return <div className="TaskPage"></div>;
+}
+
+export default TaskPage;
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,13 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import TaskPage from "./TaskPage";
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <TaskPage />
   </React.StrictMode>
 );
-
