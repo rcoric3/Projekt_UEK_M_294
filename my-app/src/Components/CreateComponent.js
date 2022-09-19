9030b274669ca44dbd237f4a1c1743a6ad067207
rcoric3 <92095914+rcoric3@users.noreply.github.com>
2022-09-19 09:14:03 +0200

    added new component

--- my-app/src/Components/CreateComponent.js
+++ my-app/src/Components/CreateComponent.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
 
 function CreateComponent() {
-  return (
-    <div>CreateComponent</div>
-  )
+  const [post, getPost] = useState([]);
+  const API = "http://localhost:3000/tasks";
+  const fetchPost = () => {
+    fetch(API)
+      .then((res) => res.json())
+      .then((res) => {
+        console.log(res);
+        getPost(res);
+      });
+  };
+  useEffect(() => {
+    fetchPost();
+  }, []);
+  return <div>CreateComponent</div>;
 }
 
-export default CreateComponent
+export default CreateComponent;
